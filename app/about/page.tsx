f604476bdc09c70125d2a117f987897b0fb9f0f8
Tuhin Ahmed <mdtuhin.dev@gmail.com>
2023-07-06 15:15:38 +0600

    ex-2

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,37 +1,37 @@
-// /* eslint-disable @next/next/no-img-element */
-// export default function About() {
-//   return (
-//     <section className="w-screen mt-12">
-//       <div className="flex flex-1 justify-start items-center flex-col gap-6">
-//         <h5 className="font-bold text-4xl">Activitis</h5>
-//         <p className="md:text-2xl xs:1xl uppercase font-light">
-//           30 MINUTE CODING
-//         </p>
-//         <p className="w-3/5 text-center md:text-[22px] xs:text-[15px] font-monospace">
-//           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam esse
-//           officiis amet ullam ratione unde.
-//         </p>
-//         <div className="flex xs:flex-col md:flex-row justify-center items-center gap-6">
-//           <img
-//             className="md:w-1/4 xs:w-3/4 shadow-lg hover:shadow-2xl"
-//             src="https://images.unsplash.com/photo-1579412690850-bd41cd0af397?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=465&q=80"
-//             alt=""
-//           />
-//           <img
-//             className="md:w-1/4 xs:w-3/4 shadow-lg hover:shadow-2xl"
-//             src="https://images.unsplash.com/photo-1618477247222-acbdb0e159b3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80"
-//             alt=""
-//           />
-//           <img
-//             className="md:w-1/4 xs:w-3/4 shadow-lg hover:shadow-2xl"
-//             src="https://images.unsplash.com/photo-1579403124614-197f69d8187b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80"
-//             alt=""
-//           />
-//         </div>
-//         <button className="border-purple-500 border-2 p-2 rounded-md">
-//           View More
-//         </button>
-//       </div>
-//     </section>
-//   );
-// }
+/* eslint-disable @next/next/no-img-element */
+export default function About() {
+  return (
+    <section className="w-screen mt-12">
+      <div className="flex flex-1 justify-start items-center flex-col gap-6">
+        <h5 className="font-bold text-4xl">Activitis</h5>
+        <p className="md:text-2xl xs:1xl uppercase font-light">
+          30 MINUTE CODING
+        </p>
+        <p className="w-3/5 text-center md:text-[22px] xs:text-[15px] font-monospace">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam esse
+          officiis amet ullam ratione unde.
+        </p>
+        <div className="flex xs:flex-col md:flex-row justify-center items-center gap-6">
+          <img
+            className="md:w-1/4 xs:w-3/4 shadow-lg hover:shadow-2xl"
+            src="https://images.unsplash.com/photo-1579412690850-bd41cd0af397?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=465&q=80"
+            alt=""
+          />
+          <img
+            className="md:w-1/4 xs:w-3/4 shadow-lg hover:shadow-2xl"
+            src="https://images.unsplash.com/photo-1618477247222-acbdb0e159b3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80"
+            alt=""
+          />
+          <img
+            className="md:w-1/4 xs:w-3/4 shadow-lg hover:shadow-2xl"
+            src="https://images.unsplash.com/photo-1579403124614-197f69d8187b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80"
+            alt=""
+          />
+        </div>
+        <button className="border-purple-500 border-2 p-2 rounded-md">
+          View More
+        </button>
+      </div>
+    </section>
+  );
+}
